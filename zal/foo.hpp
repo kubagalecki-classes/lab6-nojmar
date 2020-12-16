47c457be28d4305b258efb20d734c7354d4c43ee
nojmar <73884817+nojmar@users.noreply.github.com>
2020-12-16 18:36:17 +0100

    Update foo.hpp

--- zal/foo.hpp
+++ zal/foo.hpp
@@ -10,8 +10,8 @@ std::vector< char > foo(std::list< Human >& people)
 {
     std::vector< char > ret_v(people.size());
 
-   for_each(people.begin(),people.end(),[](Human&h){return h.birthday()});
-   transform(people.cbegin(),people.cend(),ret_v.rbegin(),[](const Human&h){
+   std::for_each(people.begin(),people.end(),[](Human&h){return h.birthday()});
+   std::transform(people.cbegin(),people.cend(),ret_v.rbegin(),[](const Human&h){
               return h.isMonster()?'n':'y';});
     
 
